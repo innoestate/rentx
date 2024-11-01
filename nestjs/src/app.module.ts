d0ce6bfa2e87a0fa33d2aaf2d555fbf87b625820
Mathieu Colla <mathieucolla@gmail.com>
2024-11-01 09:10:26 +0100

    use static route in nestjs to serve angular application

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+console.log(join(__dirname, '../angular/dist/angular'));
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../angular/dist/angular'),
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
