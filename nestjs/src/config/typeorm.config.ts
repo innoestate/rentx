7624b8a9c9132022a19ff9e55e949f622fed646f
Mathieu Colla <mathieucolla@gmail.com>
2024-11-17 11:38:11 +0100

    [back] add tests end-to-end
    create a specific docker-compose for tests
    create mock for auth JWT guard token
    create a test for user with a built user on db

--- nestjs/src/config/typeorm.config.ts
+++ nestjs/src/config/typeorm.config.ts
@@ -5,7 +5,13 @@ import { User } from './../user/user.entity';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) {
+
+    // console.log('node-env', this.configService.get<string>('NODE_ENV'));
+
+
+  }
+
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
     const config = {
@@ -16,7 +22,8 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_DATABASE'),
       entities: [User],// [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: false, // set to false in production
+      synchronize: this.configService.get<string>('NODE_ENV') === 'test', // set to false in production
+      drpopSchema: this.configService.get<string>('NODE_ENV') === 'test',
       autoLoadEntities: true,
     };
     console.log('config', config);
