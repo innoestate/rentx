0ec0e21109694ef08769d5bb05502d4a289fdfa7
Mathieu Colla <mathieucolla@gmail.com>
2024-11-17 15:35:48 +0100

    [front] add nzTable and address

--- angular/src/app/core/utils/estate.utils.ts
+++ angular/src/app/core/utils/estate.utils.ts
@@ -1,6 +1,8 @@
 import { Estate_Form } from '../models/forms/estate-form.model';
 import { Estate_Post_Request } from '../models/requests/estate-post-request.model';
 import { FormGroup } from '@angular/forms';
+import { Estate_Dto } from '../models/dtos/estate.dto.model';
+import { Estate } from '../models/estate.model';
 
 export const formatEstateToEstateFormToEstatePostRequest = (estate: FormGroup<Estate_Form>): Estate_Post_Request => {
 
@@ -14,4 +16,11 @@ export const formatEstateToEstateFormToEstatePostRequest = (estate: FormGroup<Es
     lodger: estate.get('lodger')?.value
   }
 
+}
+
+export const formatEstateDtoToEstateUx = (estate: Estate_Dto): Estate => {
+  return {
+    ...estate,
+    address: estate.street + ' ' + estate.city + ' ' + estate.zip
+  }
 }
--- angular/src/app/desktop/dashboard/pages/estates/estates.component.ts
+++ angular/src/app/desktop/dashboard/pages/estates/estates.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EstatePage } from 'src/app/common/pages/estates.page.component';
 import { CreateDesktopEstatePopupComponent } from '../../../../common/popups/create-estate-popup/create-estate-popup.component';
+import { Estate } from 'src/app/core/models/estate.model';
 
 @Component({
   selector: 'app-estates',
@@ -16,5 +17,26 @@ export class EstatesPageDesktopComponent extends EstatePage{
       nzFooter: null
     })
   }
+
+  startEdit(id: string, ref: HTMLInputElement) {
+    this.editId = id;
+    setTimeout(() => {
+      requestAnimationFrame(() => {
+        ref.focus();
+      })
+    }, 0);
+  }
+
+  stopEdit() {
+    this.editId = null;
+  }
+
+
+  edit(estate: Estate, fieldName: string, ref: HTMLInputElement) {
+    // const editableEstate: any = { id: estate.id };
+    // editableEstate[fieldName] = ref.value;
+    // this.store.dispatch({ type: '[Estates] Edit Estate', estate: editableEstate })
+  }
+
 
 }
