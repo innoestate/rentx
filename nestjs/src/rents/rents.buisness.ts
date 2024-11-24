57b10ad2a6d4b221d6354b4b522edc42fc9d2826
Mathieu Colla <mathieucolla@gmail.com>
2024-11-24 10:59:06 +0100

    [back] refacto rents buisness

--- nestjs/src/rents/rents.buisness.ts
+++ nestjs/src/rents/rents.buisness.ts
@@ -1,33 +1,41 @@
 import * as fs from 'fs';
 import * as pdfkit from 'pdfkit';
+import { Estate } from 'src/estates/estate.entity';
 
-export const createRentReciptPdf = () => {
+export const createRentReciptPdf = async () => {
 
     return new Promise((resolve, reject) => {
 
-        try {
-            const doc = new pdfkit({
-                margins: { top: 50, bottom: 50, left: 50, right: 50 }
-            });
+        const doc = initDoc();
+        runStream(doc, null, document => resolve(document));
 
-            const fileName = 'quittance' + Date.now();
-            if (!fs.existsSync('/tmp')) {
-                fs.mkdirSync('/tmp');
-            }
-            const stream = fs.createWriteStream(`/tmp/${fileName}.pdf`);
-            stream.on('finish', () => {
-                const document = fs.readFileSync(`/tmp/${fileName}.pdf`);
-                resolve(document);
-            });
-            doc.pipe(stream);
-
-            doc.text('hello world!');
-            doc.save();
-            doc.end();
-
-        } catch (err) {
-            console.error(err);
-            reject(err);
-        }
+        doc.text('hello world!');
+        
+        finish(doc);
     });
+
+}
+
+const initDoc = () => {
+    return new pdfkit({
+        margins: { top: 50, bottom: 50, left: 50, right: 50 }
+    });
+}
+
+const runStream = (doc: any, estate: Estate, callback) => {
+    const fileName = 'quittance' + Date.now();
+    if (!fs.existsSync('/tmp')) {
+        fs.mkdirSync('/tmp');
+    }
+    const stream = fs.createWriteStream(`/tmp/${fileName}.pdf`);
+    stream.on('finish', () => {
+        const document = fs.readFileSync(`/tmp/${fileName}.pdf`);
+        callback(document);
+    });
+    doc.pipe(stream);
+}   
+
+const finish = (doc) => {
+    doc.save();
+    doc.end();
 }
