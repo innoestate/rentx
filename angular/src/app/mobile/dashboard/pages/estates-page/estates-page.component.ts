d3266f201aa7bd513f79f6da40cdef64e1f745f8
Mathieu Colla <mathieucolla@gmail.com>
2024-12-05 12:35:13 +0100

    [front] mobile: add owner and lodger

--- angular/src/app/mobile/dashboard/pages/estates-page/estates-page.component.ts
+++ angular/src/app/mobile/dashboard/pages/estates-page/estates-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { EstatePage } from '../../../../common/pages/estates.page.component';
+import { CreateOwnerPopupComponent } from 'src/app/common/popups/create-owner-popup/create-owner-popup.component';
+import { CreateLodgerPopupComponent } from 'src/app/common/popups/create-lodger-popup/create-lodger-popup.component';
 
 @Component({
   selector: 'app-estates-page',
@@ -7,5 +9,21 @@ import { EstatePage } from '../../../../common/pages/estates.page.component';
   styleUrl: './estates-page.component.scss'
 })
 export class EstatesMobilePageComponent extends EstatePage {
+
+  openCreateOwner() {
+    this.modalService.create({
+      nzTitle: 'Ajouter un nouveau propriétaire',
+      nzContent: CreateOwnerPopupComponent,
+      nzFooter: null
+    })
+  }
+
+  openCreateLodger() {
+    this.modalService.create({
+      nzTitle: 'Ajouter un nouveau locataire',
+      nzContent: CreateLodgerPopupComponent,
+      nzFooter: null
+    })
+  }
 
 }
