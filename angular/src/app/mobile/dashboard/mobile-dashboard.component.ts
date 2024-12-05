9d71c65eb34f71741ea6f032dc184e06e79d0089
Mathieu Colla <mathieucolla@gmail.com>
2024-12-05 21:15:20 +0100

    [front] fix height for mobile

--- angular/src/app/mobile/dashboard/mobile-dashboard.component.ts
+++ angular/src/app/mobile/dashboard/mobile-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DashboardComponent } from '../../common/dashboard.component';
 
 @Component({
@@ -6,7 +6,12 @@ import { DashboardComponent } from '../../common/dashboard.component';
   templateUrl: './mobile-dashboard.component.html',
   styleUrl: './mobile-dashboard.component.scss'
 })
-export class MobileDashboardComponent extends DashboardComponent {
+export class MobileDashboardComponent extends DashboardComponent implements AfterViewInit {
 
+  @ViewChild('layout') layout!: ElementRef;
 
+  ngAfterViewInit() {
+    console.log('layout', this.layout);
+    this.layout.nativeElement.style.height =  window.innerHeight + 'px';
+  }
 }
