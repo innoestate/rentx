96dca51e074382a7d515f76ceb4eb832ec905f59
Mathieu Colla <mathieucolla@gmail.com>
2024-12-12 08:15:14 +0100

    [back] add buildRentReceipt by email

--- nestjs/src/rents/rents.service.ts
+++ nestjs/src/rents/rents.service.ts
@@ -1,23 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { catchError, from, Observable, switchMap } from 'rxjs';
-import { createRentReciptPdf, getRentReceiptInfos } from './rent-receipts.business';
+import { createRentReceiptEmail, createRentReciptPdf, getRentReceiptInfos } from './rent-receipts.business';
 import { RentsDbService } from './rents.db.service';
+import { Owner_Db } from 'src/owners/owners-db.model';
+import { Lodger_Db } from 'src/lodgers/lodger-db.model';
+import { Estate_Db } from 'src/estates/estate-db.model';
 
 
 @Injectable()
 export class RentsService {
 
-  constructor( private rentsDbService: RentsDbService,
+  constructor(private rentsDbService: RentsDbService,
   ) { }
 
-  
+
   buildRentReciptPdf(estate: any, owner: any, lodger: any, startDate_: string, endDate_: string): Observable<any> {
 
-    const { startDate, endDate, rent, charges, totalRent, street, ownerZipAndCity, lodgerZipAndCity, madeAt, signature } = getRentReceiptInfos(estate, owner, lodger, startDate_, endDate_);
+    const { startDate, endDate, rent, charges } = getRentReceiptInfos(estate, owner, lodger, startDate_, endDate_);
 
     return from(this.rentsDbService.create({ estate_id: estate.id, lodger_id: lodger.id, start_date: startDate, end_date: endDate, rent, charges })).pipe(
-      switchMap( rent => from(createRentReciptPdf(estate, owner, lodger, startDate_, endDate_))),
-      catchError( err => from(createRentReciptPdf(estate, owner, lodger, startDate_, endDate_)))
+      switchMap(rent => from(createRentReciptPdf(estate, owner, lodger, startDate_, endDate_))),
+      catchError(err => from(createRentReciptPdf(estate, owner, lodger, startDate_, endDate_)))
+    );
+  }
+
+  BuildRentReceiptEmail(owners: Owner_Db[], lodgers: Lodger_Db[], estate: Estate_Db, startDate_?: string, endDate_?: string) {
+
+    const owner = owners.find(owner => owner.id === estate.owner_id);
+    const lodger = lodgers.find(lodger => lodger.id === estate.lodger_id);
+
+    const { startDate, endDate, rent, charges } = getRentReceiptInfos(estate, owner, lodger, startDate_, endDate_);
+
+    return from(this.rentsDbService.create({ estate_id: estate.id, lodger_id: lodger.id, start_date: startDate, end_date: endDate, rent, charges })).pipe(
+      switchMap(rent => from(createRentReceiptEmail(owners, lodgers, estate, startDate_, endDate_))),
+      catchError(err => from(createRentReceiptEmail(owners, lodgers, estate, startDate_, endDate_)))
     );
   }
 
