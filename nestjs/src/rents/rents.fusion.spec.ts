965fc2e5ce2cada222c6e97a0074e33a6509ebac
Mathieu Colla <mathieucolla@gmail.com>
2024-12-19 19:22:54 +0100

    [back] add tests on rents fusions

--- nestjs/src/rents/rents.fusion.spec.ts
+++ nestjs/src/rents/rents.fusion.spec.ts
@@ -68,7 +68,7 @@ describe('unit test of rents fusions', () => {
 
     it('test a rent without fusion', () => {
 
-        const rents = fusionateRents([rent1]);
+        const rents = fusionateRents([{...rent1}]);
         expect(rents[0].estate_id).toEqual(rent1.estate_id);
         expect(rents[0].lodger_id).toEqual(rent1.lodger_id);
 
@@ -76,8 +76,8 @@ describe('unit test of rents fusions', () => {
 
     it('test a doublon of rent', () => {
 
-        const rents = fusionateRents([rent1, rent1Bis]);
-        expect(rents.length).toEqual(1);    
+        const rents = fusionateRents([{...rent1}, {...rent1Bis}]);
+        expect(rents.length).toEqual(1);
         expect(rents[0].estate_id).toEqual(rent1.estate_id);
         expect(rents[0].lodger_id).toEqual(rent1.lodger_id);
 
@@ -85,7 +85,7 @@ describe('unit test of rents fusions', () => {
 
     it('test 2 rents without fusion', () => {
 
-        const rents = fusionateRents([rent1, rent3]);
+        const rents = fusionateRents([{...rent1}, {...rent3}]);
         expect(rents.length).toEqual(2);
         expect(rents[0].estate_id).toEqual(rent1.estate_id);
         expect(rents[1].estate_id).toEqual(rent3.estate_id);
@@ -96,7 +96,7 @@ describe('unit test of rents fusions', () => {
 
     it('test a fusion of 2 consecutive rents', () => {
 
-        const rents = fusionateRents([rent1, rent2]);
+        const rents = fusionateRents([{...rent1}, {...rent2}]);
         expect(rents.length).toEqual(1);
         expect(rents[0].estate_id).toEqual(rent1.estate_id);
         expect(rents[0].lodger_id).toEqual(rent1.lodger_id);
@@ -107,10 +107,90 @@ describe('unit test of rents fusions', () => {
 
     it('test a fusion of 2 consecutive rents', () => {
 
-        const rents = fusionateRents([rent1, rent0]);
+        const rents = fusionateRents([{...rent1}, {...rent0}]);
         expect(rents.length).toEqual(1);
         expect(rents[0].start_date).toEqual(rent0.start_date);
         expect(rents[0].end_date).toEqual(rent1.end_date);
+
+    })
+
+    it('test a fusion of 3 consecutive rents', () => {
+
+        const rents = fusionateRents([{...rent1}, {...rent2}, {...rent3}]);
+        expect(rents.length).toEqual(1);
+        expect(rents[0].start_date).toEqual(rent1.start_date);
+        expect(rents[0].end_date).toEqual(rent3.end_date);
+
+    })
+
+    it('test a fusion of 2 consecutive rents with one without fusion', () => {
+
+        const rent2Bis = {...rent2, start_date: new Date('2021-02-02')};
+
+        const rents = fusionateRents([{...rent1}, {...rent2Bis}, {...rent3}]);
+        expect(rents.length).toEqual(2);
+        expect(rents[0].start_date).toEqual(rent1.start_date);
+        expect(rents[0].end_date).toEqual(rent1.end_date);
+        expect(rents[1].start_date).toEqual(rent2Bis.start_date);
+        expect(rents[1].end_date).toEqual(rent3.end_date);
+
+    })
+
+    it('test a fusion of 2 fusionned rents', () => {
+
+        const rent2Bis = { ...rent2, start_date: new Date('2021-02-15') };
+
+        const rents = fusionateRents([{ ...rent1 }, { ...rent0 }, { ...rent2Bis }, { ...rent3 }]);
+        expect(rents.length).toEqual(2);
+        expect(rents[0].start_date).toEqual(rent0.start_date);
+        expect(rents[0].end_date).toEqual(rent1.end_date);
+        expect(rents[1].start_date).toEqual(rent2Bis.start_date);
+        expect(rents[1].end_date).toEqual(rent3.end_date);
+
+    })
+
+
+    it('test a fusion of 2 fusionned rents', () => {
+
+        const rent2Bis = { ...rent2, start_date: new Date('2021-02-15') };
+
+        const rents = fusionateRents([{ ...rent2Bis }, { ...rent3 }, { ...rent1 }, { ...rent0 }]);
+        expect(rents.length).toEqual(2);
+        expect(rents[0].start_date).toEqual(rent0.start_date);
+        expect(rents[0].end_date).toEqual(rent1.end_date);
+        expect(rents[1].start_date).toEqual(rent2Bis.start_date);
+        expect(rents[1].end_date).toEqual(rent3.end_date);
+
+    })
+
+    it('test a fusion of 2 rents walking on together', () => {
+
+        const rent2Bis = { ...rent2, start_date: new Date('2021-01-15') };
+
+        const rents = fusionateRents([{ ...rent1 },{ ...rent2Bis }]);
+        expect(rents.length).toEqual(1);
+        expect(rents[0].start_date).toEqual(rent1.start_date);
+        expect(rents[0].end_date).toEqual(rent2Bis.end_date);
+
+    })
+
+    it('test a fusion of 1 rent englobing the other', () => {
+
+        const rent2Bis = { ...rent2, start_date: new Date('2019-01-15') };
+        const rents = fusionateRents([{ ...rent1 },{ ...rent2Bis }]);
+        expect(rents.length).toEqual(1);
+        expect(rents[0].start_date).toEqual(rent2Bis.start_date);
+        expect(rents[0].end_date).toEqual(rent2Bis.end_date);
+
+    })
+
+    it('test a fusion of 2 rent englobing the other and 2 classics', () => {
+
+        const rent2Bis = { ...rent2, start_date: new Date('2019-01-15') };
+        const rents = fusionateRents([{ ...rent1 },{...rent0},{...rent3},{ ...rent2Bis }]);
+        expect(rents.length).toEqual(1);
+        expect(rents[0].start_date).toEqual(rent2Bis.start_date);
+        expect(rents[0].end_date).toEqual(rent3.end_date);
 
     })
 
--- nestjs/src/rents/rents.utils.ts
+++ nestjs/src/rents/rents.utils.ts
@@ -1,23 +1,18 @@
 import { Rent_Db } from "./rents.db";
 
 export const fusionateRents = (rents: Rent_Db[]): Rent_Db[] => {
-    if(rents.length === 0) return [];
+    if (rents.length === 0) return [];
 
-    const fusionnedRents = [rents.shift()];
-    for (let rent of rents ){
+    rents = rents.sort((a, b) => a.start_date.getTime() - b.start_date.getTime());
 
-        const lastRent = fusionnedRents[fusionnedRents.length - 1];
-
-        if (isOneDayDifference(lastRent.end_date, rent.start_date)) {
-            lastRent.end_date = new Date(rent.end_date);
-        } else if (isOneDayDifference(lastRent.start_date, rent.end_date)) {
-            lastRent.start_date = new Date(rent.start_date);
-        }else{
-            fusionnedRents.push(rent);
-
+    const fusionnedRents = [];
+    while (rents.length > 0 ) {
+        fusionnedRents.push(rents.shift());
+        for(let fusion of fusionnedRents){
+            rents = fusionRent(fusion, rents);
         }
-
     }
+
     const uniqueRents = fusionnedRents.filter((rent, index, self) =>
         index === self.findIndex((r) => (
             r.start_date.getTime() === rent.start_date.getTime() &&
@@ -28,6 +23,26 @@ export const fusionateRents = (rents: Rent_Db[]): Rent_Db[] => {
     return uniqueRents;
 }
 
+export const fusionRent = (rent: Rent_Db, rentsToMerge: Rent_Db[]): Rent_Db[] => {
+
+    const rentsAfterMerge = [];
+    for (let rentToMerge of rentsToMerge) {
+
+        if(rentToMerge.start_date <= rent.end_date){
+            if(rentToMerge.end_date > rent.end_date){
+                rent.end_date = new Date(rentToMerge.end_date);
+                rent.start_date = rentToMerge.start_date.getTime() < rent.start_date.getTime() ? rentToMerge.start_date : rent.start_date;
+            }
+        }else if (isOneDayDifference(rent.end_date, rentToMerge.start_date)) {
+            rent.end_date = rentToMerge.end_date;
+        } else if (isOneDayDifference(rentToMerge.end_date, rent.start_date)) {
+            rent.start_date = rentToMerge.start_date;
+        } else {
+            rentsAfterMerge.push(rentToMerge);
+        }
+    }
+    return rentsAfterMerge;
+}
 
 export const isOneDayDifference = (date1: Date, date2: Date): boolean => {
     const oneDay = 24 * 60 * 60 * 1000; // milliseconds in one day
