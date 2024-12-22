2b602b31498e24d81debefbb86b466066516f2f1
Mathieu Colla <mathieucolla@gmail.com>
2024-12-22 08:59:41 +0100

    [back] clean tests

--- nestjs/src/rents/tests/rents.utils.spec.ts
+++ nestjs/src/rents/tests/rents.utils.spec.ts
@@ -1,32 +1,8 @@
 import { Rent_Db } from "../rents.db";
 import { getStartAndEnDatesFromRents } from "../rents.utils";
+import { rent2021_01, rent2021_02 } from "./rents.mocks";
 
 describe('testing geting start and end date from rents', () => {
-
-    const rent1: Rent_Db = {
-        id: '1',
-        estate_id: '1',
-        user_id: 'A',
-        lodger_id: 'B',
-        start_date: new Date('2021-01-01'),
-        end_date: new Date('2021-01-31'),
-        rent: 100,
-        charges: 10,
-        created_at: new Date(),
-        updated_at: new Date()
-    }
-    const rent2: Rent_Db = {
-        id: '2',
-        estate_id: '1',
-        user_id: 'A',
-        lodger_id: 'B',
-        start_date: new Date('2021-02-01'),
-        end_date: new Date('2021-02-28'),
-        rent: 100,
-        charges: 10,
-        created_at: new Date(),
-        updated_at: new Date()
-    }
 
     it('should return no unusedEstates', () => {
 
@@ -39,7 +15,7 @@ describe('testing geting start and end date from rents', () => {
 
     it('should return correct start and end dates', () => {
 
-        const rents: Rent_Db[] = [{...rent1}, {...rent2}];
+        const rents: Rent_Db[] = [{...rent2021_01}, {...rent2021_02}];
         expect(getStartAndEnDatesFromRents(rents).startDate).toEqual(rents[0].start_date);
         expect(getStartAndEnDatesFromRents(rents).endDate).toEqual(rents[1].end_date);
 
@@ -47,7 +23,7 @@ describe('testing geting start and end date from rents', () => {
 
     it('should return correct start and end dates with inversed rents', () => {
 
-        const rents: Rent_Db[] = [{...rent1, end_date: new Date('2025-01-01')},{...rent2, start_date: new Date('2020-01-01')}];
+        const rents: Rent_Db[] = [{...rent2021_01, end_date: new Date('2025-01-01')},{...rent2021_02, start_date: new Date('2020-01-01')}];
         expect(getStartAndEnDatesFromRents(rents).startDate).toEqual(rents[1].start_date);
         expect(getStartAndEnDatesFromRents(rents).endDate).toEqual(rents[0].end_date);
 
