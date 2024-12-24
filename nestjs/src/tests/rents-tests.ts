a3adfcdc9474a20c22013545a5456fa7fff03b5b
Mathieu Colla <mathieucolla@gmail.com>
2024-12-24 06:10:27 +0100

    [back] add test to check sent

--- nestjs/src/tests/rents-tests.ts
+++ nestjs/src/tests/rents-tests.ts
@@ -6,6 +6,28 @@ export const rentsTests = (getApp, getRentsService) => {
 
     let estate = null;
 
+    it('GET /api/rents/email', async () => {
+
+        const app = getApp();
+        const rentService = getRentsService() as RentsDbService;
+
+        const responseEstate = await request(app.getHttpServer())
+            .get('/api/estates')
+            .expect(200);
+        expect(responseEstate.body.length > 0).toBeTruthy();
+        estate = responseEstate.body[0];
+
+        const response = await request(app.getHttpServer())
+            .get('/api/rents/email?estate=' + estate.id)
+            .expect(200);
+        expect(response.body).toBeDefined();
+
+        const rent = await firstValueFrom(rentService.getByEstate(estate.id));
+        expect(rent.length).toBe(1);
+        expect(rent[0].sent).toBeTruthy();
+
+    });
+
     it('GET /api/rents/pdf', async () => {
 
         const app = getApp();
@@ -26,7 +48,6 @@ export const rentsTests = (getApp, getRentsService) => {
         expect(rent.length).toBe(1);
 
     });
-
 
     it('GET /api/rents/pdf new rent erase if exists', async () => {
 
@@ -63,6 +84,35 @@ export const rentsTests = (getApp, getRentsService) => {
         const rents = response.body;
         expect(rents.length).toBe(1);
         expect(rents[0].rents[0].rent).toBe(2000);
+        expect(!!rents[0].rents[0].sent).toBe(false);
+
+
+    });
+
+    it('GET /api/rents/email', async () => {
+
+        const app = getApp();
+        const rentService = getRentsService() as RentsDbService;
+
+        const responseEstate = await request(app.getHttpServer())
+            .get('/api/estates')
+            .expect(200);
+        expect(responseEstate.body.length > 0).toBeTruthy();
+        estate = responseEstate.body[0];
+
+        const response = await request(app.getHttpServer())
+            .get('/api/rents/email?estate=' + estate.id)
+            .expect(200);
+        expect(response.body).toBeDefined();
+
+        const rentsResponse = await request(app.getHttpServer())
+        .get('/api/rents')
+        .expect(200);
+        expect(rentsResponse.body).toBeDefined();
+
+        const rent = rentsResponse.body[0].rents;
+        expect(rent.length).toBe(1);
+        expect(rent[0].sent).toBeTruthy();
 
     });
 
