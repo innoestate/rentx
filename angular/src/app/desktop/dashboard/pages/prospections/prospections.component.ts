43f8d378e5f09bafad8c3f5556f80d6c91595dae
Mathieu Colla <mathieucolla@gmail.com>
2024-12-28 09:23:34 +0100

    [front] add creation popup

--- angular/src/app/desktop/dashboard/pages/prospections/prospections.component.ts
+++ angular/src/app/desktop/dashboard/pages/prospections/prospections.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { loadProspections } from 'src/app/core/store/prospections.actions';
 import { selectAllProspections } from 'src/app/core/store/prospections.selectors';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { CreateProspectionComponent } from 'src/app/common/popups/create-prospection/create-prospection.component';
 
 @Component({
   selector: 'app-prospections',
@@ -12,8 +14,16 @@ export class ProspectionsDesktopComponent {
 
   prospections = this.store.selectSignal(selectAllProspections);
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private modalService: NzModalService) {
     this.store.dispatch(loadProspections());
+  }
+
+  openCreatePropspectionPopup() {
+    this.modalService.create({
+      nzTitle: 'Create Prospection',
+      nzContent: CreateProspectionComponent,
+      nzFooter: null
+    });
   }
 
 }
