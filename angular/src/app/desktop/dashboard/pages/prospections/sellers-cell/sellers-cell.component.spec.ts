e8e05ad937a4c35bde192ecef6235617fb4edefe
Mathieu Colla <mathieucolla@gmail.com>
2025-01-09 18:35:30 +0100

    [front] add test for opening createSeller popup

--- angular/src/app/desktop/dashboard/pages/prospections/sellers-cell/sellers-cell.component.spec.ts
+++ angular/src/app/desktop/dashboard/pages/prospections/sellers-cell/sellers-cell.component.spec.ts
@@ -6,37 +6,47 @@ import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { ProspectionStoreFacade } from 'src/app/core/facade/prospection.store.facade';
 import { SellersCellComponent } from './sellers-cell.component';
-
+import { By } from '@angular/platform-browser';
+import { CreateSellerPopupComponent } from './../../../../../common/popups/create-seller-popup/create-seller-popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
+import { delay } from 'rxjs';
 
 const prospection = {
-  seller: {
-    id: 1,
-    name: 'John Doe',
-    email: 'zvE0w@example.com',
-  }
+  seller: null
 }
 
 describe('SellersCellComponent', () => {
+
   let component: SellersCellComponent;
   let fixture: ComponentFixture<SellersCellComponent>;
+  let modalService: NzModalService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SellersCellComponent],
       providers: [
         { provide: ProspectionStoreFacade, useClass: ProspectionMockedFacade },
+        NzModalService
       ],
       imports: [
+        BrowserAnimationsModule,
+        CommonModule,
         NzInputNumberModule,
+        ReactiveFormsModule,
         NzIconModule,
         NzDropDownModule,
-        NzSelectModule
+        NzSelectModule,
+        NzModalModule
       ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(SellersCellComponent);
     component = fixture.componentInstance;
+    modalService = TestBed.inject(NzModalService);
     fixture.componentRef.setInput('prospection', prospection);
     fixture.detectChanges();
     await fixture.whenStable();
@@ -46,4 +56,21 @@ describe('SellersCellComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(('should have an empty seller class'), () => {
+    expect(fixture.nativeElement.querySelector('.empty-field')).toBeTruthy();
+  });
+
+  it('should open the modal for creating a new seller', async () => {
+    const button = fixture.nativeElement.querySelector('.create');
+    const openSpy = spyOn(modalService, 'create').and.callThrough();
+
+    button.click();
+    fixture.detectChanges();
+
+    expect(openSpy).toHaveBeenCalledWith(jasmine.objectContaining({
+      nzContent: CreateSellerPopupComponent
+    }));
+  });
+
 });
--- angular/src/app/desktop/dashboard/pages/prospections/sellers-cell/sellers-cell.component.ts
+++ angular/src/app/desktop/dashboard/pages/prospections/sellers-cell/sellers-cell.component.ts
@@ -1,4 +1,6 @@
 import { Component, input } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { CreateSellerPopupComponent } from 'src/app/common/popups/create-seller-popup/create-seller-popup.component';
 import { ProspectionStoreFacade } from 'src/app/core/facade/prospection.store.facade';
 import { Prospection } from 'src/app/core/models/prospection.model';
 import { Seller } from 'src/app/core/models/seller.model';
@@ -13,14 +15,18 @@ export class SellersCellComponent {
   prospection = input.required<Prospection>();
   sellers = this.prospectionFacade.getSellers();
 
-  constructor(private prospectionFacade: ProspectionStoreFacade) { }
+  constructor(private prospectionFacade: ProspectionStoreFacade, private nzModalService: NzModalService) { }
 
   setSeller(seller: Seller) {
     this.prospectionFacade.setSeller(this.prospection(), seller);
   }
 
   create() {
-
+    this.nzModalService.create({
+      nzTitle: 'Create Seller',
+      nzContent: CreateSellerPopupComponent,
+      nzFooter: null
+    });
   }
 
 }
