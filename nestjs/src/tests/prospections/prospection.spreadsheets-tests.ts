6b98958934e27b085b14b3332de05055fd79013a
Mathieu Colla <mathieucolla@gmail.com>
2025-01-26 13:46:20 +0100

    [back] add tests e2e for creation of prospections

--- nestjs/src/tests/prospections/prospection.spreadsheets-tests.ts
+++ nestjs/src/tests/prospections/prospection.spreadsheets-tests.ts
@@ -1,8 +1,11 @@
 import { firstValueFrom } from 'rxjs';
 import { DocsDbService } from '../../docs/docs.db.service';
 import * as request from 'supertest';
+import { ProspectionMocked1, ProspectionMocked2 } from '../../prospections/spreadsheets/tests/mocks/prospections.mocked';
 
 export const prospectionsSpreadsheetTests = (getApp, getUser, getDocsDbService) => {
+
+    let spreadsheetId;
 
     it('should create a spreadsheet with one prospection', async () => {
 
@@ -10,52 +13,42 @@ export const prospectionsSpreadsheetTests = (getApp, getUser, getDocsDbService) 
         const user = getUser();
         const docsDbService = getDocsDbService() as DocsDbService;
 
+        const prospectionMockedDto = {...ProspectionMocked1};
+        delete prospectionMockedDto.id;
+        delete prospectionMockedDto.seller_id;
+        delete prospectionMockedDto.offer_id;
+
+
         await request(app.getHttpServer())
             .post('/api/prospections')
-            .send({
-                city: 'Test City',
-                address: 'Test Address',
-                price: 100000,
-                emission_date: new Date().toISOString(),
-            })
+            .send(prospectionMockedDto)
             .expect(201);
 
         const docs = await firstValueFrom(docsDbService.getByUser(user.id));
         expect(docs?.length).toBe(1);
         expect(docs[0].prospections_google_sheet_id).toBeTruthy();
+        spreadsheetId = docs[0].prospections_google_sheet_id;
 
     })
 
     it('should get the prospection spreadsheet', async () => {
-        expect(true).toBe(true);
-    })
+        const app = getApp();
+        const user = getUser();
+        const docsDbService = getDocsDbService() as DocsDbService;
 
-    it('prospection spreadsheet should have correct sheets and titles', async () => {
-        expect(true).toBe(true);
-    })
+        const prospectionMockedDto = {...ProspectionMocked2};
+        delete prospectionMockedDto.id;
+        delete prospectionMockedDto.seller_id;
+        delete prospectionMockedDto.offer_id;
 
-    it('prospection spreadsheet should have correct headers and rows', async () => {
-        expect(true).toBe(true);
-    })
+        await request(app.getHttpServer())
+            .post('/api/prospections')
+            .send(prospectionMockedDto)
+            .expect(201);
 
-    it('should add a row in prospections sheet', async () => {
-        expect(true).toBe(true);
-    })
-
-    it('should add a row in sellers sheet', async () => {
-        expect(true).toBe(true);
-    })
-
-    it('should add a row in prospections sheet', async () => {
-        expect(true).toBe(true);
-    })
-
-    it('should add multiple prospecions without doublons', async () => {
-        expect(true).toBe(true);
-    })
-
-    it('should set the removed prospection in archives sheet', async () => {
-        expect(true).toBe(true);
+        const docs = await firstValueFrom(docsDbService.getByUser(user.id));
+        expect(docs?.length).toBe(1);
+        expect(docs[0].prospections_google_sheet_id).toEqual(spreadsheetId);
     })
 
 }
