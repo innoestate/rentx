1b0f208c4a50450ce86fc1d86581b4dcb1b36474
Mathieu Colla <mathieucolla@gmail.com>
2025-02-15 17:49:26 +0100

    [front] add environment variable for baseUrl

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
+  baseUrl: 'https://rentx.fr',
   apiURL: 'https://rentx.fr/api',
 }
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
+  baseUrl: 'http://localhost:4200',
   apiURL: 'http://localhost:3000/api',
 }
