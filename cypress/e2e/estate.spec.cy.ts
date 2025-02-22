f0b1060130ec6c6fcbd8fa3c7e5e5234b27f2792
Mathieu Colla <mathieucolla@gmail.com>
2025-02-22 06:46:53 +0100

    [front] update cypress test selectors

--- cypress/e2e/estate.spec.cy.ts
+++ cypress/e2e/estate.spec.cy.ts
@@ -8,13 +8,13 @@ describe('estate page', function () {
   })
 
   it('add estate from main menu', function () {
-    cy.get('[data-cy="add-estate-main-button"]').click()
+    cy.get('[test-selector="add-estate-main-button"]').click()
     cy.get('[data-cy="create-estate-form"]').should('be.visible'); // Ensure modal is visible before interacting
     cy.get('[data-cy="create-estate-street"]').focus().type('123 Main St')
     cy.get('[data-cy="create-estate-city"]').focus().type('Anytown')
     cy.get('[data-cy="create-estate-zip"]').focus().type('12345')
     cy.get('[data-cy="create-estate-plot"]').focus().type('1A')
-    cy.get('[data-cy="create-estate-submit"]').click()
+    cy.get('[test-selector="create-estate-submit"]').click()
     cy.get('.anticon-close').click()
     cy.get('[data-cy="estate-owner-cell"]').should('be.visible')
     cy.get('[data-cy="estate-owner-cell"]').invoke('text').should('eq', johnDoe.firstName + ' ' + johnDoe.lastName)
