35f37c2a9ae544f587fabb66be0ec67919a1a6ab
Mathieu Colla <mathieucolla@gmail.com>
2025-03-02 17:09:53 +0100

    [front] add loading-value in table editable number

---
+++ angular/src/app/ui/components/ui-table/nz-ui-cell-editable/number/test/number.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { NzUxCellEditableNumberComponent } from "../number.component";
+
+describe('NzUxCellEditableNumberComponent unit test', () => {
+
+  let fixture: ComponentFixture<NzUxCellEditableNumberComponent>;
+  let component: NzUxCellEditableNumberComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NzUxCellEditableNumberComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NzUxCellEditableNumberComponent);
+    fixture.componentRef.setInput('value', 10);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+  it('should have a specific class when loading value', fakeAsync(() => {
+
+    expect(component.value()).toBe(10);
+    let clickableElement = fixture.debugElement.query(By.css('.clickable')).nativeElement;
+    expect(clickableElement.classList.contains('loading-value')).toBeFalsy();
+
+    const input = fixture.debugElement.query(By.css('input')).nativeElement;
+    input.value = 20;
+    input.dispatchEvent(new Event('change'));
+
+    tick(500);
+    fixture.detectChanges();
+    expect(clickableElement.classList.contains('loading-value')).toBeTruthy();
+
+    fixture.componentRef.setInput('value', 20);
+    tick(500);
+    fixture.detectChanges();
+    expect(component.value()).toBe(20);
+    expect(clickableElement.classList.contains('loading-value')).toBeFalsy();
+
+  }))
+
+});
