fa084f29d7a6da053c92ca36bbc3e13bc3c5adb5
Mathieu Colla <mathieucolla@gmail.com>
2025-03-04 18:32:47 +0100

    [front] add updatedField method in ownersTableAdapter

---
+++ angular/src/app/owners/adapters/table/test/mocks/owners.mock.ts
@@ -0,0 +1,11 @@
+export const ownerMock1 = {
+  id: '1',
+  user_id: 'user1',
+  signature: '',
+  name: 'John Doe',
+  street: '123 Main St',
+  zip: '12345',
+  city: 'Anytown',
+  email: 'john@example.com',
+  phone: '123-456-7890'
+};
