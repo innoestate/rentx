c61a231a8348ca6bf7c7e91e0cb2fcc85395a900
Mathieu Colla <mathieucolla@gmail.com>
2025-04-09 11:52:07 +0200

    [front] add smooth animation on hover

--- angular/src/app/ui/components/ui-navigation/ui-navigation.component.ts
+++ angular/src/app/ui/components/ui-navigation/ui-navigation.component.ts
@@ -1,12 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { UiNavigatorComponent } from '../ui-navigator/ui-navigator.component';
 import { UiNavigator } from '../ui-navigator/model/ui-navigator.model';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'ui-navigation',
   imports: [UiNavigatorComponent],
   templateUrl: './ui-navigation.component.html',
-  styleUrl: './ui-navigation.component.scss'
+  styleUrls: ['./ui-navigation.component.scss'],
+  animations: [
+    trigger('sizeChange', [
+      transition('* <=> *', [
+        animate('300ms cubic-bezier(0.4, 0, 0.2, 1)', style('*'))
+      ])
+    ])
+  ],
+  host: {
+    '[@sizeChange]': ''
+  }
 })
 export class UiNavigationComponent {
 
