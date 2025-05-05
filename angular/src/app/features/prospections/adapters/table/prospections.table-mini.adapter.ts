f5bafa275ebffec9a011c51a34c172a3e9b42078
Mathieu Colla <mathieucolla@gmail.com>
2025-05-05 11:14:46 +0200

    [front] update mini table

--- angular/src/app/features/prospections/adapters/table/prospections.table-mini.adapter.ts
+++ angular/src/app/features/prospections/adapters/table/prospections.table-mini.adapter.ts
@@ -4,6 +4,7 @@ import { UiTableRow } from "src/app/ui/components/ui-table/models/ui-table-row.m
 import { Prospection_Dto } from "../../models/prospection.dto.model";
 import { UiTableColumn } from "src/app/ui/components/ui-table/models/ui-table.column.model";
 import { Prospection } from "../../models/prospection.model";
+import { PROSPECTION_STATUS, ProspectionStatus } from "../../models/prospection.status.model";
 
 @Injectable({
   providedIn: 'root'
@@ -34,6 +35,13 @@ export class ProspectionsTableMiniAdapterService {
           type: 'string',
           label: { title: { label: 'Vendeur' } }
         }
+      },
+      {
+        key: 'status',
+        cell: {
+          type: 'string',
+          label: { title: { label: 'Statut' } }
+        }
       }
     ]
   }
@@ -45,23 +53,36 @@ export class ProspectionsTableMiniAdapterService {
         data: { id: prospection.id || '' },
         cells: {
           address: {
-            editable: true,
             type: 'string',
             label: { title: { label: prospection.address || '' } }
           },
           price: {
-            editable: true,
             type: 'number',
             label: { title: { label: prospection.price?.toString() || '' } }
           },
           seller: {
-            editable: true,
             type: 'string',
             label: { title: { label: prospection.seller?.name || '' } }
+          },
+          status: {
+            type: 'string',
+            label: { icon: {
+              name: this.getStatus(prospection)?.icon || '',
+              size: 20,
+              color: "var(--color-basic-100)",
+            }, color: this.getStatus(prospection)?.color || '', title: { label: this.getStatus(prospection)?.shortLabel || '', color: "var(--color-basic-100)" } }
           }
         }
       })
     )
+  }
+
+  private getStatusShortLabel(prospection: Prospection): string {
+    return PROSPECTION_STATUS.find(status => status.key === prospection.status)?.shortLabel || '';
+  }
+
+  private getStatus(prospection: Prospection): ProspectionStatus | undefined {
+    return PROSPECTION_STATUS.find(status => status.key === prospection.status);
   }
 
   selectRow(row: UiTableRow) {}
